73de44f1572afeffaaffdeee79c3e093fac31101
toulim <toulim0525@ewhain.net>
2023-12-18 16:31:15 +0900

    글 생성 - controller method

--- src/main/java/me/urim/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/urim/springbootdeveloper/controller/BlogApiController.java
@@ -1,4 +1,27 @@
 package me.urim.springbootdeveloper.controller;
 
+import lombok.RequiredArgsConstructor;
+import me.urim.springbootdeveloper.domain.Article;
+import me.urim.springbootdeveloper.dto.AddArticleRequest;
+import me.urim.springbootdeveloper.service.BlogService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController // HTTP Response body에 객체 데이터를 Json 형식으로 반환하는 컨트롤러
 public class BlogApiController {
+
+    private final BlogService blogService;
+
+    @PostMapping("/api/articles")
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest req) {
+        Article savedArticle = blogService.save(req);
+
+        // 요청 자원이 성공적으로 생성되었으며, 저장된 블로그 글 정보를 응답 객체에 담아 전송
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(savedArticle);
+    }
 }
