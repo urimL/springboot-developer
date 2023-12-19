675dfb774e724edc39d03a1036c450e2ad19aa1b
toulim <toulim0525@ewhain.net>
2023-12-19 21:55:23 +0900

    글 목록 조회 - controller method

--- src/main/java/me/urim/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/urim/springbootdeveloper/controller/BlogApiController.java
@@ -3,12 +3,16 @@ package me.urim.springbootdeveloper.controller;
 import lombok.RequiredArgsConstructor;
 import me.urim.springbootdeveloper.domain.Article;
 import me.urim.springbootdeveloper.dto.AddArticleRequest;
+import me.urim.springbootdeveloper.dto.ArticleResponse;
 import me.urim.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController // HTTP Response body에 객체 데이터를 Json 형식으로 반환하는 컨트롤러
@@ -23,5 +27,18 @@ public class BlogApiController {
         // 요청 자원이 성공적으로 생성되었으며, 저장된 블로그 글 정보를 응답 객체에 담아 전송
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        // GET 요청이 오면 글 전체를 조회하는 findAll() 메서드를 호출한다.
+        // 응답용 객체인 ArticleResponse로 파싱해 body에 담아 클라이언트에게 전송한다.
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article (title,content) VALUEW ('1','1')
+INSERT INTO article (title,content) VALUEW ('2','2')
+INSERT INTO article (title,content) VALUEW ('3','3')
