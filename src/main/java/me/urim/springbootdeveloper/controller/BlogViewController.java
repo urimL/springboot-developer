75e1bdee4ddc644399edf10bcb8f0d6e469ed54f
toulim <toulim0525@ewhain.net>
2023-12-23 17:34:28 +0900

    글 수정 기능 추가

--- src/main/java/me/urim/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/me/urim/springbootdeveloper/controller/BlogViewController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -38,4 +39,22 @@ public class BlogViewController {
 
         return "article";
     }
+
+    @GetMapping("/new-article")
+    //id를 가진 쿼리 파라미터의 값을 id 변수에 매핑 (id가 없을수도 있다!)
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        //id가 없는 경우 생성해야 된다.
+        if (id==null) {
+            model.addAttribute("article",new ArticleViewResponse());
+        }
+
+        //id가 있는 경우, 수정해준다.
+        else {
+             Article article = blogService.findById(id);
+             model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";
+    }
+
 }
--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -16,3 +16,29 @@ if (deleteButton) {
         });
     });
 }
+
+//수정 기능
+//id가 modify-btn인 엘리먼트 조회
+const modifyButton = document.getElementById('modify-btn');
+
+if (modifyButton) {
+    modifyButton.addEventListener('click', event => {
+        let params = new URLSearchParams(location.search);
+        let id = params.get('id');
+
+        fetch(`/api/articles/${id}`, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: document.getElementById('title').value,
+                content: document.getElementById('content').value
+            })
+        })
+        .then(() => {
+            alert('수정이 완료되었습니다.');
+            location.replace(`/articles/${id}`);
+        });
+    });
+}
